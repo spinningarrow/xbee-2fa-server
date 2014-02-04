a874603bfd6453effa0c690c1aee17ff80ff3a7c
Sahil Bajaj <sahil29@gmail.com>
2014-02-04 23:11:37 +0800

    Set up server-XBee communication
    * Completed extremely basic version using a modified xbee-api example
      file to broadcast LED on/off packets every 5 seconds

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
+var sys = require('sys')
+var exec = require('child_process').exec
+
 var express = require('express')
 var app = express()
 
 app.get('/', function(req, res) {
 	res.send('Hello, world!')
+})
+
+app.get('/led', function(req, res) {
+	exec('cd xbee_api_jar && java -jar xbee-api.jar', function () {
+
+	})
+	res.send('LED stuff')
 })
 
 app.listen(4000)
