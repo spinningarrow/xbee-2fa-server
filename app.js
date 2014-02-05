96975467581577e8c4616f6e398a7d203a8d4313
Sahil Bajaj <sahil29@gmail.com>
2014-02-06 02:07:05 +0800

    Add comments to app.js and create LED on/off action

--- app.js
+++ app.js
@@ -7,15 +7,41 @@ var express = require('express')
 var app = express()
 
 // Routes
-app.get('/', function(req, res) {
-	res.send('Hello, world!')
+
+// Index route; do nothing useful
+app.get('/', function(request, response) {
+	response.send('Hello, world!')
 })
 
-app.get('/led', function(req, res) {
-	exec('cd xbee_api_jar && java -jar xbee-api.jar', function () {
+// User login
+// Server checks credentials, establishes secure channel
 
+// Server verifies OTA is legitimate and sends OTA key of sensor Node
+// to device
+
+// Sensor node sends details of device + 2FA code request
+
+// Mobile device sends detauls of OTA and 2FA code request to server
+
+// Server verifies legitimate OTA and sends 2FA code to sensor
+
+// Server verifies legitimate OTA and sends 2FA code to mobile device
+// via email or SMS
+
+// Test route (test LED on/off broadcast)
+app.get('/led/:action', function(request, response) {
+
+	// Get the desired action string and capitalise it
+	var ledAction = request.params.action.toLowerCase()
+	ledAction = ledAction.charAt(0).toUpperCase() + ledAction.slice(1)
+
+	var command = 'cd xbee_api_jar && java -jar xbee-api.jar -led' + ledAction
+
+	exec(command, function (error, stdout, stderr) {
+		sys.print(stdout)
 	})
-	res.send('LED stuff')
+
+	response.send('LED turned ' + ledAction)
 })
 
 // Init
